6853e478dab04327bd2ec7d0d8f82f255c417af0
Denis Agniel <dagniel@rand.org>
2019-01-15 16:58:23 -0600

    Fix bug and commit notebook.

--- test.R
+++ test.R
@@ -13,12 +13,3 @@ print(installed_pkgs)
 ##   * Priority
 ##   * Built
 
-## write this data frame to data/installed-packages.csv
-## hint: readr::write_csv() or write.table()
-## idea: try using here::here() to create the file path
-
-write_csv(installed_pkgs, here('data', 'installed-packages.csv'))
-
-## YES overwrite the file that is there now (or delete it first)
-## that's a old result from me (Jenny)
-## it an example of what yours should look like and where it should go
